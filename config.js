521d7242dc2c469b5a1f707b43b369ce8a49e7b0
TrungTis <88956231+TrungTis@users.noreply.github.com>
2021-08-15 18:51:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hello Thúy!',
+    introDesc: `T có điều muốn hỏi Thúy nhớ phải trả lời thật lòng nhaaa
+    
+    
+    `,
+    btnIntro: 'Okila',
+    title: 'Thúy yêu T không nào',
+    desc: 'nếu Thúy không trả lời mà thoát ra tức là muốn làm chồng T rồi đó nha=))',
+    btnYes: 'yêu ơi là yêu<3',
+    btnNo: 'M mơ à',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Thúy lại thik T <3',
+    btnReply: 'Gửi cho T<3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <333',
+    mess: 'T biết mà^^ yêu Thúy nhiều:3',
+    messDesc: 'Vậy từ giờ trở đi chúng ta là người yêu của nhau rồi nha:3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://facebook.com/TrungTisNe251003' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
